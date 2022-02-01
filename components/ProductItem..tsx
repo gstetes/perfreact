d22d4b291cf58d8e468f0db094c25fa22f0e1aca
Guilherme Stetes <guilherme@dobesone.com.br>
2022-02-01 14:07:30 -0300

    01022022

--- components/ProductItem..tsx
+++ components/ProductItem..tsx
@@ -1,15 +1,29 @@
+import { memo } from 'react';
+
 interface ProductItemProps {
   product: {
     id: number;
     price: number;
+    priceFormatted: string;
     title: string;
   };
+  onAddToWishList: (id: number) => void;
 }
 
-export function ProductItem({ product }: ProductItemProps) {
+function ProductItemComponent({ product, onAddToWishList }: ProductItemProps) {
   return (
     <div>
-      {product.title} - <strong>{product.price}</strong>
+      {product.title} - <strong>{product.priceFormatted}</strong>
+      <button onClick={() => onAddToWishList(product.id)}>
+        Add to Wish List
+      </button>
     </div>
   );
 }
+
+export const ProductItem = memo(
+  ProductItemComponent,
+  (prevProps, nextProps) => {
+    return Object.is(prevProps.product, nextProps.product);
+  },
+);
--- components/SearchResults.tsx
+++ components/SearchResults.tsx
@@ -1,18 +1,34 @@
+import { useMemo } from 'react';
 import { ProductItem } from './ProductItem.';
 
 interface SearchResultsProps {
+  totalPrice: number;
   results: Array<{
     id: number;
     price: number;
+    priceFormatted: string;
     title: string;
   }>;
+  onAddToWishList: (id: number) => void;
 }
 
-export function SearchResults({ results }: SearchResultsProps) {
+export function SearchResults({
+  results,
+  onAddToWishList,
+}: SearchResultsProps) {
+  const totalPrice = useMemo(() => {}, [results]);
+
   return (
     <div>
+      <h2> {totalPrice} </h2>
       {results.map((product) => {
-        return <ProductItem product={product} />;
+        return (
+          <ProductItem
+            key={product.id}
+            product={product}
+            onAddToWishList={onAddToWishList}
+          />
+        );
       })}
     </div>
   );
